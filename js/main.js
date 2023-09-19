9b002a1121255aab0a784bc08b73464fa07be089
Nikmet <metlov.n@mail.ru>
2023-09-19 21:29:19 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector(".header-nav");
+const menuBtn = document.querySelector(".nav-btn");
+const menuBtnImg = document.querySelector(".nav-btn-img");
+
+menuBtn.addEventListener("click", () => {
+    if (menu.classList.toggle("open")) {
+        menuBtnImg.src = "../assets/nav-close.svg";
+    } else {
+        menuBtnImg.src = "../assets/nav.svg";
+    }
+});
+
+AOS.init({
+    once: true
+});
